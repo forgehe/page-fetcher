a92c5f661123772125a01b890a8bd15eb03f0f37
Richard Liao <liao098@gmail.com>
2020-02-13 00:36:52 +0000

    initial commit

---
+++ fetcher.js
@@ -0,0 +1,11 @@
+const request = require("request");
+const fs = require("fs");
+const args = process.argv.slice(2);
+
+const fetcher = args => {
+  //stuff
+  const url = args[0];
+  const saveTo = args[1];
+};
+
+fetcher(args);
